7a1bb1fbbe9ab0d019603a99f72a63c6e53a00b8
dmish66 <mitkomishinov@abv.bg>
2025-03-05 15:27:41 +0300

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import { QueryProvider } from './lib/react-query/QueryProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
+    <HashRouter>
         <QueryProvider>
             <AuthProvider>
                 <App />
             </AuthProvider>
         </QueryProvider>
-    </BrowserRouter>
+    </HashRouter>
 )
