b5fa09ff0e0c35ed52c5947461abe55eb970cc46
dmish66 <mitkomishinov@abv.bg>
2025-03-05 15:31:19 +0300

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import { QueryProvider } from './lib/react-query/QueryProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <HashRouter>
+    <BrowserRouter basename="/peace-link">
         <QueryProvider>
             <AuthProvider>
                 <App />
             </AuthProvider>
         </QueryProvider>
-    </HashRouter>
+    </BrowserRouter>
 )
